230b53a4a262737f22d5773d1ed41d9b640dc8b9
justinavickers <justinav38@gmail.com>
2019-02-01 10:39:10 -0600

    posting lego creations into the json file

---
+++ practice/collectingUserInput.js
@@ -0,0 +1,23 @@
+document.querySelector(".saveBTN").addEventListener("click", event => {
+  const creator = document.querySelector("#lego__creator").value
+  const color = document.getElementById("lego__color").value
+  const shape = document.getElementById("lego__shape").value
+  const creation = document.getElementById("lego__creation").value
+
+  const legoToSave = {
+      lego__creator: creator,
+      lego__color: color,
+      lego__shape: shape,
+      lego__creation: creation
+  }
+
+  fetch("http://localhost:3000/legocreations",{
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(legoToSave)
+
+  })
+})
+
